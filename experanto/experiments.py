a2ab032ce86047f81912bdc739a991dcd9b29410
goirik-chakrabarty <chakrabarty.goirik@gmail.com>
2025-12-02 14:35:37 +0100

    Added Experanto experiment

---
+++ experanto/experiments.py
@@ -0,0 +1,205 @@
+#!/usr/local/bin/ipython -i
+from mozaik.experiments import *
+from mozaik.experiments.vision import *
+from mozaik.experiments.optogenetic import *
+from mozaik.sheets.population_selector import RCRandomPercentage
+from parameters import ParameterSet
+from copy import deepcopy
+
+def create_experiments(model):
+    return [
+        # Spontaneous Activity
+        NoStimulation(model, ParameterSet(
+            {'duration': 3*8*2*5*3*8*7})),
+
+        # Measure orientation tuning with full-filed sinusoidal gratins
+        MeasureOrientationTuningFullfield(model, ParameterSet(
+        #    {'num_orientations': 1, 'spatial_frequency': 0.8, 'temporal_frequency': 2, 'grating_duration': 2*143*7, 'contrasts': [100], 'num_trials':10, 'shuffle_stimuli': True})),
+            {'num_orientations': 10, 'spatial_frequency': 0.8, 'temporal_frequency': 2, 'grating_duration': 2*143*7, 'contrasts': [10,30,100], 'num_trials':10, 'shuffle_stimuli': True})),
+
+        # Measure response to natural image with simulated eye movement
+        MeasureNaturalImagesWithEyeMovement(model, ParameterSet(
+            {'stimulus_duration': 2*143*7, 'num_trials': 10, 'size':30, 'shuffle_stimuli': False})),
+    ]
+
+
+def create_experiments_stc(model):
+    return [
+        # Spontaneous Activity
+        NoStimulation(model, ParameterSet({'duration': 2*5*3*8*7})),
+
+        # Size Tuning
+        MeasureSizeTuning(model, ParameterSet({'num_sizes': 12, 'max_size': 5.0, 'log_spacing': True, 'orientations': [0], 'positions': [(0,0)],
+                                               'spatial_frequency': 0.8, 'temporal_frequency': 2, 'grating_duration': 2*143*7, 'contrasts': [10, 100], 'num_trials': 10, 'shuffle_stimuli': True})),
+    ]
+
+def create_experiments_spont(model):
+    return [
+        # Spontaneous Activity
+        NoStimulation(model, ParameterSet(
+            {'duration': 1001})),
+    ]
+
+def create_experiments_video(model):
+    return [
+        # Spontaneous Activity
+        MeasurePixelMovieExperanto(model, ParameterSet(
+            {
+            'base_path': '/data/dynamic29513-my-test',
+            'movie_name': '',
+            "width" : 11,
+            "movie_frame_duration" : 7,
+            "global_frame_offset" : 0,
+            "images_per_trial" : 300,
+            "num_presentation_trials" : 1,
+            "num_trials": 4,
+            "shuffle_stimuli": False,
+            "video_max_value": 255.0,
+            })),
+    ]
+
+def create_experiments_spont_STW(model):
+    return [
+        # Spontaneous Activity
+        NoStimulation(model, ParameterSet(
+            {'duration': 2*8*5*3*3*4*7})),
+    ]
+
+def create_experiments_spont_MSA(model):
+    return [
+        # Spontaneous Activity
+        NoStimulation(model, ParameterSet(
+            {'duration': 120 * 1001})),
+    ]
+
+def patterned_opt_stim(model, images_path, intensity, num_trials=5, sheet="V1_Exc_L2/3"):
+    experiments = []
+    mulholland_parameters = {
+        "sheet_list": [sheet],
+        "sheet_intensity_scaler": [1.0],
+        "sheet_transfection_proportion": [1.0],
+        "num_trials": num_trials,
+        "stimulator_array_parameters": {
+            "size": 4000,
+            "spacing": 10.0,
+            "depth_sampling_step": 10,
+            "light_source_light_propagation_data": "light_scattering_radial_profiles_lsd10.pickle",
+            "update_interval": 1,
+        },
+        "intensities": [intensity],
+        "duration": 4000,
+        "onset_time": 1000,
+        "offset_time": 2000,
+    }
+    p_full = MozaikExtendedParameterSet(deepcopy(mulholland_parameters))
+    p_full.images_path = images_path
+    experiments.append(OptogeneticArrayImageStimulus(model, p_full))
+    return experiments
+
+# We match the stim intensity ratio of 7.6 mW/mm^2 and 10 mW/mm^2 based on
+# H. N. Mulholland, H. Jayakumar, D. M. Farinella, G. B. Smith, All-optical interrogation of millimeter-scale networks and application to developing ferret cortex. J. Neurosci. Methods 403, 110051 (2024).
+# and
+# H. N. Mulholland, M. Kaschube, G. B. Smith, Self-organization of modular activity in immature cortical networks. Nat. Commun. 15, 4145 (2024).
+def create_experiments_optogenetic_fullfield_stimulation(model):
+    return patterned_opt_stim(model, "optogenetic_stimulation_patterns/fullfield.npy", 0.033, 40)
+
+def create_experiments_optogenetic_endogenous_stimulation(model):
+    return patterned_opt_stim(model, "optogenetic_stimulation_patterns/endogenous", 0.033 * 0.76, 5)
+
+def create_experiments_optogenetic_surrogate_stimulation(model):
+    return patterned_opt_stim(model, "optogenetic_stimulation_patterns/surrogate", 0.033 * 0.76, 5)
+
+def create_experiments_central_stimulation(model):
+    intensities = [9,2.25,1,0.5625,0.36,0.25]
+    radii = [50,100,150,200,250,300]
+    experiments = []
+    for i in range(len(intensities)):
+        experiments.append(
+            SingleOptogeneticArrayStimulus(
+                model,
+                MozaikExtendedParameterSet(
+                    {
+                        "sheet_list": ["V1_Exc_L2/3"],
+                        'sheet_intensity_scaler': [1.0],
+                        'sheet_transfection_proportion': [1.0],
+                        "num_trials": 10,
+                        "stimulator_array_parameters": MozaikExtendedParameterSet(
+                            {
+                                "size": 1000,
+                                "spacing": 10,
+                                "depth_sampling_step": 10,
+                                "light_source_light_propagation_data": "light_scattering_radial_profiles_lsd10.pickle",
+                                "update_interval": 1,
+                            }
+                        ),
+                        "stimulating_signal": "mozaik.sheets.direct_stimulator.stimulating_pattern_flash",
+                        "stimulating_signal_parameters": ParameterSet(
+                            {
+                                "shape": "circle",
+                                "coords": [[0,0]],
+                                "radius": radii[i],
+                                "intensity": intensities[i],
+                                "duration": 4000,
+                                "onset_time": 1000,
+                                "offset_time": 2000,
+                            }
+                        ),
+                    }
+                ),
+            )
+    )
+    return experiments
+
+def fullfield_gratings_8_orientations(model):
+    return [
+        # Spontaneous Activity
+        NoStimulation(model, ParameterSet(
+            {'duration': 1001 * 5})),
+
+        MeasureOrientationTuningFullfield(model, ParameterSet(
+            {'num_orientations': 8, 'spatial_frequency': 0.8, 'temporal_frequency': 2, 'grating_duration': 2*143*7, 'contrasts': [100], 'num_trials':10, 'shuffle_stimuli': True})),
+    ]
+
+def create_experiments_chernov_visual_opto(model):
+    return [
+            NoStimulation(model, ParameterSet({'duration': 1001 * 3})),
+            OptogeneticArrayStimulusCircleWithFullfieldSquareGrating(
+            model,
+            MozaikExtendedParameterSet(
+                {
+                    "sheet_list": ["V1_Exc_L2/3"],
+                    'sheet_intensity_scaler': [1.0],
+                    'sheet_transfection_proportion': [1.0],
+                    "num_trials": 10,
+                    "stimulator_array_parameters": MozaikExtendedParameterSet(
+                        {
+                            "size": 400,
+                            "spacing": 10,
+                            "depth_sampling_step": 10,
+                            "light_source_light_propagation_data": "light_scattering_radial_profiles_lsd10.pickle",
+                            "update_interval": 1,
+                        }
+                    ),
+                    "intensities": [4.5],
+                    "radii": [100],
+                    "x_center": 0,
+                    "y_center": 0,
+                    "inverted": False,
+                    "duration": 4000,
+                    "onset_time": 0,
+                    "offset_time": 600,
+                    "orientations": [0,np.pi/2],
+                    "spatial_frequency": 1,
+                    "temporal_frequency": 4,
+                    "contrasts": [100],
+                    "shuffle_stimuli": False,
+                }
+            ),
+        )]
+
+def create_experiments_chernov_visual(model):
+    return [
+        NoStimulation(model, ParameterSet({'duration': 1001 * 3})),
+        MeasureFrequencySensitivity(model, ParameterSet({"orientation":0,'spatial_frequencies':[1],'temporal_frequencies' : [4],'grating_duration' : 4*1001,'contrasts' : [100],'num_trials' : 10,'square' : True,"shuffle_stimuli": False})),
+        MeasureFrequencySensitivity(model, ParameterSet({"orientation":np.pi/2,'spatial_frequencies':[1],'temporal_frequencies' : [4],'grating_duration' : 4*1001,'contrasts' : [100],'num_trials' : 10,'square' : True, "shuffle_stimuli": False}))
+    ]
---
+++ experanto/run.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+This is the implementation of the model corresponding to the pre-print `Iso-orientation bias of layer 2/3 connections: the unifying mechanism of spontaneous, visually and optogenetically driven V1 dynamics`
+Rózsa, T., Cagnol, R., Antolík, J. (2024).
+https://www.biorxiv.org/ TODO: Update
+"""
+import sys
+import matplotlib
+matplotlib.use('Agg')
+
+from mpi4py import MPI
+from mozaik.storage.datastore import Hdf5DataStore, PickledDataStore
+from parameters import ParameterSet
+#from analysis_and_visualization import perform_analysis_and_visualization_spont
+from model import SelfSustainedPushPull
+from experiments import create_experiments_video
+import mozaik
+print(">>>>>>>>>>>>>>>>", mozaik.__file__)
+
+from mozaik.controller import run_workflow, setup_logging
+import mozaik.controller
+import sys
+from pyNN import nest
+
+from mpi4py import MPI
+
+mpi_comm = MPI.COMM_WORLD
+
+import nest
+nest.Install("stepcurrentmodule")
+
+if True:
+    data_store, model = run_workflow(
+        'SelfSustainedPushPull', SelfSustainedPushPull, create_experiments_video)
+    if False:
+        model.connectors['V1AffConnectionOn'].store_connections(data_store)
+        model.connectors['V1AffConnectionOff'].store_connections(data_store)
+        model.connectors['V1AffInhConnectionOn'].store_connections(data_store)
+        model.connectors['V1AffInhConnectionOff'].store_connections(data_store)
+        model.connectors['V1L4ExcL4ExcConnection'].store_connections(
+           data_store)
+        model.connectors['V1L4ExcL4InhConnection'].store_connections(
+            data_store)
+        model.connectors['V1L4InhL4ExcConnection'].store_connections(
+            data_store)
+        model.connectors['V1L4InhL4InhConnection'].store_connections(
+            data_store)
+        model.connectors['V1L23ExcL23ExcConnection'].store_connections(
+            data_store)
+        model.connectors['V1L23ExcL23InhConnection'].store_connections(
+            data_store)
+        model.connectors['V1L23InhL23ExcConnection'].store_connections(
+            data_store)
+        model.connectors['V1L23InhL23InhConnection'].store_connections(
+            data_store)
+        model.connectors['V1L4ExcL23ExcConnection'].store_connections(data_store)
+        model.connectors['V1L4ExcL23InhConnection'].store_connections(data_store)
+    data_store.save()
+else:
+    setup_logging()
+    data_store = PickledDataStore(load=True, parameters=ParameterSet(
+        {'root_directory': 'SelfSustainedPushPull_test____', 'store_stimuli': False}), replace=True)
+
+#if mpi_comm.rank == 0:
+#    print("Starting visualization")
+#    perform_analysis_and_visualization_spont(data_store)
